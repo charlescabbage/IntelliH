fcb0fdc336fcd0ac15086a5b06c88d58333d0093
charlescabbage <franztopacio9@gmail.com>
2017-12-03 07:32:53 +0800

    fixed intellih.php

--- intellih.php
+++ intellih.php
@@ -9,22 +9,38 @@ if (empty($_POST['type']) || empty($_POST['mac'])) {
 
 $type=$_POST['type'];
 $mac=$_POST['mac'];
+$subj="";
+$msg="";
 
-if ($type == 1) {
-	mail('intellih@gmail.com','Warning','Fire','from: intellih@gmail.com');
-} else if ($type == 2) {
-	mail('intellih@gmail.com','Warning','Flood','from: intellih@gmail.com');
-} else if ($type == 3) {
-	mail('intellih@gmail.com','Warning','Earthquake','from: intellih@gmail.com');
-} else if ($type == 4) {
-	mail('intellih@gmail.com','Warning','Home Invasion','from: intellih@gmail.com');
-}
+$hQuery = $mysqli->query("SELECT * FROM user WHERE device_mac='$mac'");
 
-if ($type > 0) {
-	$hQuery = $mysqli->query("SELECT * FROM user WHERE device_mac='$mac'");
-	if ($hQuery->num_rows == 1) {
-		$row = $hQuery->fetch_assoc();
-		$id = $row['id'];
+if ($hQuery->num_rows == 1) {
+
+	$row = $hQuery->fetch_assoc();
+	$id = $row['id'];
+
+	if ($type == 1) {
+		$subj="IntelliH: Fire Warning";
+		$msg="A fire has been detected!";
+	} else if ($type == 2) {
+		$subj="IntelliH: Flood Warning";
+		$msg="A flood has been detected!";
+	} else if ($type == 3) {
+		$subj="IntelliH: Earthquake Warning";
+		$msg="An earthquake has been detected!";
+	} else if ($type == 4) {
+		$subj="IntelliH: Home Invasion Warning";
+		$msg="Someone invaded your home!";
+	}
+
+	$hQueryB = $mysqli->query("SELECT * FROM contacts WHERE userid='$id'");
+	for ($i = 0; $i < $hQueryB->num_rows; $i++) {
+		$row = $hQueryB->fetch_assoc();
+		// send emails to contacts
+		mail($row['email'],$subj,$msg,'from: '.WEBMASTER_EMAIL);
+	}
+
+	if ($type > 0) {
 		$mysqli->query("INSERT INTO casualties VALUES (NULL, NOW(), NOW(), '$type', '$id')");
 	}
 }
