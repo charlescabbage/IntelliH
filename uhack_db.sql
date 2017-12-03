abd471358b29f0f3be3e6560b402805e1b9f8c78
charlescabbage <franztopacio9@gmail.com>
2017-12-03 09:55:03 +0800

    updated db

--- uhack_db.sql
+++ uhack_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2017 at 08:49 PM
+-- Generation Time: Dec 03, 2017 at 02:51 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -43,7 +43,15 @@ CREATE TABLE `casualties` (
 INSERT INTO `casualties` (`id`, `date`, `time`, `type`, `userid`) VALUES
 (1, '2017-12-02', '04:07:09', 1, 1),
 (2, '2017-12-02', '04:07:09', 2, 2),
-(4, '2017-12-02', '04:07:09', 2, 1);
+(4, '2017-12-02', '04:07:09', 2, 1),
+(5, '2017-12-03', '06:00:54', 1, 1),
+(6, '2017-12-03', '06:21:52', 1, 3),
+(7, '2017-12-03', '08:08:12', 1, 2),
+(8, '2017-12-03', '08:22:52', 2, 1),
+(9, '2017-12-03', '08:23:57', 2, 1),
+(10, '2017-12-03', '08:24:24', 2, 1),
+(11, '2017-12-03', '08:24:57', 2, 1),
+(12, '2017-12-03', '08:25:08', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -55,16 +63,17 @@ CREATE TABLE `contacts` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `userid` int(11) NOT NULL
+  `userid` int(11) NOT NULL,
+  `phone_no` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contacts`
 --
 
-INSERT INTO `contacts` (`id`, `name`, `email`, `userid`) VALUES
-(3, 'lkjasdf', 'lksjadf@gmail.com', 2),
-(4, 'test', 'y@y.com', 1);
+INSERT INTO `contacts` (`id`, `name`, `email`, `userid`, `phone_no`) VALUES
+(4, 'test', 'y@y.com', 1, '0'),
+(8, 'asdfasdf', 'assadfdf@asdf.com', 2, '09213');
 
 -- --------------------------------------------------------
 
@@ -83,7 +92,8 @@ CREATE TABLE `device` (
 
 INSERT INTO `device` (`id`, `mac`) VALUES
 (1, '123'),
-(2, '321');
+(2, '321'),
+(3, '1234');
 
 -- --------------------------------------------------------
 
@@ -107,7 +117,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `email`, `password`, `salt`, `first_name`, `last_name`, `device_mac`) VALUES
 (1, 'x@x.com', '5d8e138c9eeb2c203d685ef266d48c8b', '2807546895a227653334c18.02771899', 'test', 'lodi', '321'),
-(2, 'asdf@asdf.com', 'd04a64b495634a696949416f3ef6d9c7', '20384012375a22c5844b0ec8.96768026', 'asdf', 'asdf', '123');
+(2, 'asdf@asdf.com', 'd04a64b495634a696949416f3ef6d9c7', '20384012375a22c5844b0ec8.96768026', 'asdf', 'asdf', '123'),
+(3, 'a@a.com', '9e9c89e1c50cea735031bf1677fcafc0', '14106170655a232743d3b149.02071906', 'lkh', 'lkjsdf', '1234');
 
 -- --------------------------------------------------------
 
@@ -172,25 +183,25 @@ ALTER TABLE `warnings`
 -- AUTO_INCREMENT for table `casualties`
 --
 ALTER TABLE `casualties`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `device`
 --
 ALTER TABLE `device`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `warnings`
